fe35adf7b2ff6651797a2bf61539f2a7ed305f26
André Felipe <adrfelipe.sz@gmail.com>
2023-08-15 12:30:29 -0300

    Create sidebar and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { Post } from './Post'
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar"
+
+import styles from "./App.module.css";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="André" 
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem aliquam consequuntur ullam saepe molestias odio, perferendis ratione quasi, ea sunt praesentium sint voluptatum modi? Ea culpa ad accusantium aliquid sed." 
-      />
-      <Post
-        author="João"
-        content="Um texto bem top"
-      />
-    </div>
-  )
-}
+      <Header />
 
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="André"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem aliquam consequuntur ullam saepe molestias odio, perferendis ratione quasi, ea sunt praesentium sint voluptatum modi? Ea culpa ad accusantium aliquid sed."
+          />
+          <Post author="João" content="Um texto bem top" />
+        </main>
+      </div>
+    </div>
+  );
+}
